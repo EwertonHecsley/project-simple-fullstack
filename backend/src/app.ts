2b7c38e6a28a7fcd1e9af688fde68e818306ec6d
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2024-01-11 21:42:54 -0300

    simples estilizacao, funcionalidades com banco de dados, conectadas

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import 'express-async-errors';
+import cors from 'cors';
 import router from './routes/user.routes';
 import { httpErrorMiddleware } from './middlewares/Http.Exception';
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
